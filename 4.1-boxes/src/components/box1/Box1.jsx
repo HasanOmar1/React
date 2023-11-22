72e8371b55c7a4d017b8f44a2e4c7600cdfd0903
HasanOmar1 <r3domar1@gmail.com>
2023-11-22 12:13:21 +0200

    added

---
+++ 4.1-boxes/src/components/box1/Box1.jsx
@@ -0,0 +1,9 @@
+import Box2 from "../box2/Box2"
+export default function Box1(){
+    return (
+    
+       <div className='box1'>
+       <Box2 />
+       </div>
+       )
+  }
---
+++ 4.1-boxes/src/components/box2/Box2.jsx
@@ -0,0 +1,10 @@
+import Box3 from "../box3/box3"
+
+export default function Box2(){
+    return ( 
+    <div className='box2'>
+    <Box3 />
+    </div>
+    
+    )
+  }
