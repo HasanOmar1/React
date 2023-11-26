d871b8d2c466d0d209cd2f6384b3a1efefcfe103
HasanOmar1 <r3domar1@gmail.com>
2023-11-26 11:07:06 +0200

    added

--- 6.1-increment/src/App.jsx
+++ 6.1-increment/src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 
 function App() {
 
+  const [counter , setCounter] = useState(0)
+
+  function incrementCounter(){
+    setCounter(counter + 1)
+  }
   return (
     <>
-      
+      <h1>{counter}</h1>
+      <button onClick={incrementCounter}>increment</button>
     </>
   )
 }
