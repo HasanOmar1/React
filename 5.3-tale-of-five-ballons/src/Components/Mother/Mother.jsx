985b02d6ffb56408c44ac529f766190f671e9beb
HasanOmar1 <r3domar1@gmail.com>
2023-11-26 09:33:29 +0200

    added

--- 5.3-tale-of-five-ballons/src/Components/Mother/Mother.jsx
+++ 5.3-tale-of-five-ballons/src/Components/Mother/Mother.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import Child from '../Child/Child'
 
 export default function Mother() {
-    const kids= [
-        { name: 'Ori', color: 'red' },
-        { name: 'Ron', color: 'blue' },
-        { name: 'Sigalit', color: 'green' },
-        { name: 'Ruti', color: 'yellow' },
-        { name: 'Alon', color: 'purple' },
-        ]
+  const kids= [
+      { name: 'Ori', color: 'red' },
+      { name: 'Ron', color: 'blue' },
+      { name: 'Sigalit', color: 'green' },
+      { name: 'Ruti', color: 'yellow' },
+      { name: 'Alon', color: 'purple' },
+      ]
  
 
   return (
     <div>
         <h1>A Tale of Five Balloons</h1>
         <div className="container">
-          {kids.map((kid)=>{
-            return <Child>{kid}</Child>
+          {kids.map((kid , index)=>{
+            return <Child key={index}>{kid}</Child>
           })}
         </div>
 
