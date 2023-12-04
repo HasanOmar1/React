a3ac6f113f04719919a7d813a8dd21ac8451bf23
HasanOmar1 <r3domar1@gmail.com>
2023-12-04 14:37:15 +0200

    added

--- 13.2-box-animation/src/App.jsx
+++ 13.2-box-animation/src/App.jsx
@@ -1,33 +1,13 @@
-import { useEffect } from "react";
 import "./App.css";
-import { useRef } from "react";
+import Box from "./Box";
 
 function App() {
-  const box1 = useRef();
-  const box2 = useRef();
-  const box3 = useRef();
-  const box4 = useRef();
-
-  useEffect(() => {
-    setTimeout(() => {
-      box1.current.classList.add("slide");
-      box2.current.classList.add("slide");
-      box3.current.classList.add("slide");
-      box4.current.classList.add("slide");
-      setTimeout(() => {
-        box1.current.style.visibility = "hidden";
-        box2.current.style.visibility = "hidden";
-        box3.current.style.visibility = "hidden";
-        box4.current.style.visibility = "hidden";
-      }, 4000);
-    }, 1000);
-  }, []);
   return (
     <>
-      <div className="box1" ref={box1}></div>
-      <div className="box2" ref={box2}></div>
-      <div className="box3" ref={box3}></div>
-      <div className="box4" ref={box4}></div>
+      <Box width={100} height={100} color={"green"} />
+      <Box width={90} height={90} color={"red"} />
+      <Box width={80} height={80} color={"yellow"} />
+      <Box width={70} height={70} color={"black"} />
     </>
   );
 }
