efa3504b29ba4f3aee408c89f59230624b07830f
HasanOmar1 <r3domar1@gmail.com>
2023-12-05 15:58:33 +0200

    added

--- 14.2-avatar/src/App.jsx
+++ 14.2-avatar/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import Card from "./components/Card";
-import Input from "./components/Input";
 
 function App() {
   const [fetchedData, setFetchedData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  const [inputVal, setInputVal] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -11,7 +12,7 @@ function App() {
         const response = await fetch("https://randomuser.me/api/?results=10");
         const data = await response.json();
         setFetchedData(data.results);
-        console.log(data.results);
+        setOriginalData(data.results);
       } catch (error) {
         console.log(error);
       }
@@ -19,23 +20,39 @@ function App() {
     fetchData();
   }, []);
 
-  const cards = () => {
-    return fetchedData.map((data, i) => {
-      return (
-        <Card
-          key={i}
-          name={data.name.first}
-          lastName={data.name.last}
-          img={data.picture.large}
-        />
-      );
-    });
-  };
+  useEffect(() => {
+    if (inputVal.length === 0) {
+      setFetchedData(originalData);
+    } else {
+      const filteredNames = fetchedData.filter((data) => {
+        return (
+          data.name.first.toLowerCase().includes(inputVal.toLowerCase()) ||
+          data.name.last.toLowerCase().includes(inputVal.toLowerCase())
+        );
+      });
+      return setFetchedData(filteredNames);
+    }
+  }, [inputVal]);
+
+  function handleOnChange(e) {
+    setInputVal(e.target.value);
+  }
 
   return (
     <>
-      <Input />
-      <div className="container">{cards()}</div>
+      <input type="text" value={inputVal} onChange={handleOnChange} />
+      <div className="container">
+        {fetchedData.map((data, i) => {
+          return (
+            <Card
+              key={i}
+              name={data.name.first}
+              lastName={data.name.last}
+              img={data.picture.large}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
